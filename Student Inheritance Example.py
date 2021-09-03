690645c30f5e452c8caa6bdfe25c48a2f84ea667
xavierdejesus <xavier.dejesus@gmail.com>
2021-09-03 09:54:28 +0200

    Version 8 : School Inheritance Working Students indicating working students inheriting students

--- Student Inheritance Example.py
+++ Student Inheritance Example.py
@@ -8,16 +8,19 @@ class Student:
         return sum(self.marks) / len(self.marks)
 
 
-class WorkingStudent:
+class WorkingStudent(Student):
     def __init__(self, name, school, salary):
-        self.name = name
-        self.school = school
-        self.marks = []
+        super().__init__(name, school)
         self.salary = salary
 
-    def average(self):
-        return sum(self.marks) / len(self.marks)
+    def weekly_salary(self):
+        return self.salary * 37.5
+
 
 
 rolf = WorkingStudent('Rolf', 'MIT', 15.50)
 print(rolf.salary)
+rolf.marks.append(57)
+rolf.marks.append(99)
+print(rolf.average())
+print(rolf.weekly_salary())
