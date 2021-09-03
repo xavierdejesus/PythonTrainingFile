05437a05664b36cff620447a0dfbf3b4a6777e84
xavierdejesus <xavier.dejesus@gmail.com>
2021-09-03 09:15:47 +0200

    Version 3: __repr__ and __str__ example

--- Garage.py
+++ Garage.py
@@ -8,6 +8,12 @@ class Garage:
     def __getitem__(self, i):
         return self.cars[i]
 
+    def __repr__(self):
+        return f'<Garage {self.cars}>'
+
+    def __str__(self):
+        return f'Garage with {len(self)} cars.'
+
 ford = Garage()
 ford.cars.append('Fiesta')
 ford.cars.append('Focus')
@@ -19,3 +25,5 @@ ford.cars.append('Bakie2')
 
 for car in ford:
     print(car)
+
+print(ford)
