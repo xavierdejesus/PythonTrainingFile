f69274f5a91ee4f8114d5cd07561f5f9315dc9b9
xavierdejesus <xavier.dejesus@gmail.com>
2021-09-03 09:29:51 +0200

    Version : School Inheritance Working Students as a seperate class with average method (method is a function inside a class)

--- Student Inheritance Example.py
+++ Student Inheritance Example.py
@@ -6,3 +6,18 @@ class Student:
 
     def average(self):
         return sum(self.marks) / len(self.marks)
+
+
+class WorkingStudent:
+    def __init__(self, name, school, salary):
+        self.name = name
+        self.school = school
+        self.marks = []
+        self.salary = salary
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+
+rolf = WorkingStudent('Rolf', 'MIT', 15.50)
+print(rolf.salary)
