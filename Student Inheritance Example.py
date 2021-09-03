14dc48de889205e46637013518efcc9204e3a163
xavierdejesus <xavier.dejesus@gmail.com>
2021-09-03 09:24:44 +0200

    Version 6: Added School Inheritance example

---
+++ Student Inheritance Example.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
