403e68225575fee1a98d10dbf7d94544f4e8edeb
xavierdejesus <xavier.dejesus@gmail.com>
2021-09-03 09:08:47 +0200

    Garage Class Example

---
+++ Garage.py
@@ -0,0 +1,21 @@
+class Garage:
+    def __init__(self):
+        self.cars = []
+
+    def __len__(self):
+        return len(self.cars)
+
+    def __getitem__(self, i):
+        return self.cars[i]
+
+ford = Garage()
+ford.cars.append('Fiesta')
+ford.cars.append('Focus')
+ford.cars.append('Siera')
+ford.cars.append('Bakie')
+ford.cars.append('Bakie1')
+
+
+
+for car in ford:
+    print(car)
